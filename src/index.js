0874cc776b26a471870cdb7ee3060787af5c3c50
Victor Sada <victorjsada@gmail.com>
2021-09-10 01:07:59 -0300

    create user model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const test = require("./config/databse");
+const sequelize = require("./config/database");
 
-test();
+//check if database connection has been established
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
+//check if table was created
+require("./models/user");
 
 app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () => {
