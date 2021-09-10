dbd461a46cb77741133f68d3005e063dd80430fc
Salomon Sada <sajosaca@gmail.com>
2021-09-09 22:27:07 -0300

    conectar DB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-const express = require("express");
+const express = require('express');
+require('dotenv').config();
 const app = express();
+const test = require('./config/databse');
+
+test();
 
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), ()=> {
-    console.log('Server is up on port ', app.get('port'));
-})
+app.listen(app.get('port'), () => {
+  console.log('Server is up on port ', app.get('port'));
+});
