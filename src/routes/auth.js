62f30d5761e3c36ad504af552871b201dd2bd64e
Salomon Sada <sajosaca@gmail.com>
2021-09-15 20:00:58 -0300

    rebase auth with master due conflict

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const { getAuth } = require('../controllers/authController');
+const { getAuth, login } = require('../controllers/authController');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 // @route   GET api/auth
 // @desc    Get user by JWT
 // @access  Public
 router.get('/', auth, getAuth);
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  login
+);
 
 module.exports = router;
